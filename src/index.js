034ac92f9dec0e60b873c4b6ba0df3ae7f2f97d6
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2023-02-20 10:51:33 -0500

    Configure redux store and provide it to App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
@@ -9,9 +11,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
